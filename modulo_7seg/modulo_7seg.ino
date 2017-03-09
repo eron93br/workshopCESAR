8c4da9e2ba1a5c4e9daea396f9bee1405bc82e39
Erick Simões <erick.simoes@live.com>
2017-03-08 23:23:54 -0300

    Correção da identação do código do modulo_7seg

--- modulo_7seg/modulo_7seg.ino
+++ modulo_7seg/modulo_7seg.ino
@@ -1,8 +1,8 @@
-
-
-// WORKSHOP INTRODUCAO A ARDUINO  @CESAR -- 09/03/2017
-// CODIGO PARA O MODULO DISPLAY DE SETE SEGMENTOS
-
+/*
+ * WORKSHOP INTRODUCAO A ARDUINO @CESARBR -- 09/03/2017
+ * @ErickSimoes @eron93br
+ * Código para o módulo Display de Sete Segmentos
+ */
 
 // Cria uma matriz de acionamento para cada segmento do Display Ando Comum
 // Caso o Display seja Catodo Comum inverta o "0" por "1" e o "1" por "0"
@@ -24,10 +24,7 @@ byte matriz_sete_seg [16][7] = { { 0,1,1,1,0,0,0 }, // = Digito F
 { 0,0,0,0,0,0,0 } // = Digito 0
 };
 
-
-
-void setup()
-{
+void setup() {
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT); //Pino 3 do Arduino ligado ao segmento B
   pinMode(4, OUTPUT); //Pino 4 do Arduino ligado ao segmento C
@@ -36,44 +33,41 @@ void setup()
   pinMode(7, OUTPUT); //Pino 7 do Arduino ligado ao segmento F
   pinMode(8, OUTPUT); //Pino 8 do Arduino ligado ao segmento G
   pinMode(9, OUTPUT); //Pino 9 do Arduino ligado ao segmento PONTO
-  acionaPonto(0); //Função que inicia com o ponto desligado
+  acionaPonto(0);	  //Função que inicia com o ponto desligado
 }
 
 
 // FUNÇÕES //
 //Função que aciona o ponto no display
-void acionaPonto(byte ponto)
-{
+void acionaPonto(byte ponto) {
   digitalWrite(9, ponto);
 }
 //Função que aciona os dígitos do display
-void acionaSegmentos(byte digito)
-{
-byte pino = 2;
-//Percorre a matriz ligando os segmentos correspondentes ao dígito
-for (byte contadorSeg = 0; contadorSeg < 7; ++contadorSeg)
-{
-  digitalWrite(pino, matriz_sete_seg[digito][contadorSeg]);
-  ++pino;
+void acionaSegmentos(byte digito) {
+	byte pino = 2;
+	//Percorre a matriz ligando os segmentos correspondentes ao dígito
+	for (byte contadorSeg = 0; contadorSeg < 7; ++contadorSeg) {
+		digitalWrite(pino, matriz_sete_seg[digito][contadorSeg]);
+		++pino;
+	}
+	
+	acionaPonto(1); //Liga o ponto
+	delay(100); //Aguarda 100 milisegundos
+	acionaPonto(0); //Desliga o ponto
 }
 
-acionaPonto(1); //Liga o ponto
-delay(100); //Aguarda 100 milisegundos
-acionaPonto(0); //Desliga o ponto
-}
 ///////////
 // LOOP //
 ///////////
 
-void loop()
-{
+void loop() {
 //Contador de 0 a 15, ligando os segmentos correspondentes
 //0 a 9 = liga os segmentos correspondentes aos números
 //10 a 15 = forma as letras A,B,C,D,E,F
-for (byte contador = 0; contador < 16; contador++)
-{
-delay(500);
-acionaSegmentos(contador);
+	for (byte contador = 0; contador < 16; contador++) {
+		delay(500);
+		acionaSegmentos(contador);
+	}
+	delay(2000); //Aguarda 2 segundos para reiniciar o ciclo
 }
-delay(2000); //Aguarda 2 segundos para reiniciar o ciclo
-}
+
