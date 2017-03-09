3cfbb8d3f1f7f9954373fb48ee46019a21ba366e
Erick Simões <erick.simoes@live.com>
2017-03-08 23:24:20 -0300

    Correção da identação do código do modulo_buzzer

--- modulo_buzzer/modulo_buzzer.ino
+++ modulo_buzzer/modulo_buzzer.ino
@@ -1,27 +1,20 @@
-
-// WORKSHOP INTRODUCAO A ARDUINO  @CESAR -- 09/03/2017
-// CODIGO PARA O MODULO buzzer/LED
-
-/* Módulo BUZZER 5V
-* Produzindo sons com um BUZZER conectado a um pino digital do
-* Arduino.
-*/
-
+/*
+ * WORKSHOP INTRODUCAO A ARDUINO @CESARBR -- 09/03/2017
+ * @ErickSimoes @eron93br
+ * Código para o Módulo Nuzzer/LED
+ */
 
 /////////////////////////////////
 // INICIALIZAÇÃO DAS VARIÁVEIS //
 /////////////////////////////////
 int pinoBuzzer = 13;//inicializando uma variável denominada
-//pinoBuzzer no pino digital 13 do Arduino.
-
+					//pinoBuzzer no pino digital 13 do Arduino.
 
 ///////////
 // SETUP //
 ///////////
 
-
-void setup()
-{
+void setup() {
   pinMode(pinoBuzzer, OUTPUT);//configurando o pino13(pinoBuzzer)
   pinMode(7, INPUT); // entrada
 }
@@ -31,15 +24,12 @@ void setup()
 // LOOP //
 ///////////
 
-void loop()
-{
-
+void loop() {
   tone(pinoBuzzer, 1000); //gerando no pino13 digital
   delay(500); //mantém o tom no Buzzer por 1/2s.
   noTone(pinoBuzzer); //interrompendo o tom no pino13
-  //digital denominado como
-  //pinoBuzzer.
+  					  //digital denominado como
+  					  //pinoBuzzer.
   delay(500); //aguarda 0,25s com o Buzzer desligado.
-  
 }
 
