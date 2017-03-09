01ffde6c9dfa20f07010bb00fbc577e5b7042791
Erick Simões <erick.simoes@live.com>
2017-03-08 23:25:14 -0300

    Correção da identação do código do modulo_temp

--- modulo_temp/modulo_temp.ino
+++ modulo_temp/modulo_temp.ino
@@ -1,10 +1,11 @@
+/*
+ * WORKSHOP INTRODUCAO A ARDUINO @CESARBR -- 09/03/2017
+ * @ErickSimoes @eron93br
+ * Código para o Módulo Sensor de Temperatura
+ */
 
-// WORKSHOP INTRODUCAO A ARDUINO  @CESAR -- 09/03/2017
-// CODIGO PARA O MODULO SENSOR DE TEMPERATURA
-
-
-
-#include <Thermistor.h> //inicializa a biblioteca responsável por realizar
+#include <Thermistor.h>
+//Inicializa a biblioteca responsável por realizar
 //os cálculos de conversão da temperatura,
 //considerando um termistor de 10K. Caso possua outro
 //valor de termistor, será necessário abrir o arquivo
@@ -13,7 +14,7 @@
 
 
 Thermistor temp(0); //cria variável do tipo Thermistor para ler o pino
-//de entrada analógido (A0) do Arduino
+					//de entrada analógido (A0) do Arduino
 
 float temperatura; //variável para armazenar o valor da temperatura
 
@@ -21,22 +22,20 @@ float temperatura; //variável para armazenar o valor da temperatura
 // SETUP //
 ///////////
 
-void setup()
-{
-Serial.begin(9600); //setando a comunicação via porta
-//serial à uma velocidade de
-//9600bps(baud)
+void setup() {
+	Serial.begin(9600); //setando a comunicação via porta
+						//serial à uma velocidade de
+						//9600bps(baud)
 }
 
 ///////////
 // LOOP //
 ///////////
 
-void loop()
-{
-temperatura = temp.getTemp();
-Serial.print("Temperatura = "); //imprime a palavra Temperatura =
-Serial.println(temperatura); //imprime a temperatura na tela
-delay(1000); //aguarda por 1s para a próxima leitura de temperatura.
+void loop() {
+	temperatura = temp.getTemp();
+	Serial.print("Temperatura = "); //imprime a palavra Temperatura =
+	Serial.println(temperatura); //imprime a temperatura na tela
+	delay(1000); //aguarda por 1s para a próxima leitura de temperatura.
 }
 
