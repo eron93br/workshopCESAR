88a09173dd7736bbf1649900545ee91c6a3f85c5
Erick Simões <erick.simoes@live.com>
2017-03-08 23:25:44 -0300

    Correção da identação do código de proximidade_leds

--- proximidade_leds/proximidade_leds.ino
+++ proximidade_leds/proximidade_leds.ino
@@ -1,56 +1,54 @@
-// WORKSHOP INTRODUCAO A ARDUINO  @CESAR -- 09/03/2017
-// CODIGO PARA O SENSOR DE PROXIMIDADE COM LEDs....
+/*
+ * WORKSHOP INTRODUCAO A ARDUINO @CESARBR -- 09/03/2017
+ * @ErickSimoes @eron93br
+ * Código para o Senor de Proximidade com LEDs
+ */
 
 int VERMV = 9;
 int AMARV = 10;
 int VERDV = 11;
 int VERMP = 12;
 int VERDP = 13;
+
 int SENSOR = A0;
 int valor;
 
-
-
-void setup()
-{
-                 pinMode(VERMV, OUTPUT);
-                 pinMode(AMARV, OUTPUT);
-                 pinMode(VERDV, OUTPUT);
-                 pinMode(VERDP, OUTPUT);
+void setup() {
+	pinMode(VERMV, OUTPUT);
+	pinMode(AMARV, OUTPUT);
+	pinMode(VERDV, OUTPUT);
+	pinMode(VERDP, OUTPUT);
 }
 
-void loop()
-{
-  // Configura todos os LEDs para estarem desligados.....
-  digitalWrite(VERMV, LOW);
-  digitalWrite(AMARV, LOW);
-  digitalWrite(VERDV, HIGH);
-  digitalWrite(VERMP, HIGH);
-  digitalWrite(VERDP, LOW);
-
-  // realiza leitura do sensor de proximidade! 
-  valor = analogRead(SENSOR);
+void loop() {
+	// Configura todos os LEDs para estarem desligados
+	digitalWrite(VERMV, LOW);
+	digitalWrite(AMARV, LOW);
+	digitalWrite(VERDV, HIGH);
+	digitalWrite(VERMP, HIGH);
+	digitalWrite(VERDP, LOW);
+	
+	// realiza leitura do sensor de proximidade
+	valor = analogRead(SENSOR);
  
-
-if (valor < 500) 
-{
-    delay (1000);
-
-    digitalWrite(VERMV, LOW);
-    digitalWrite(AMARV, HIGH);
-    digitalWrite(VERDV, LOW);
-    digitalWrite(VERMP, HIGH);
-    digitalWrite(VERDP, LOW);
-
-    delay (1500);
-
-    digitalWrite(VERMV, HIGH);
-    digitalWrite(AMARV, LOW); 
-    digitalWrite(VERDV, LOW);
-    digitalWrite(VERMP, LOW);
-    digitalWrite(VERDP, HIGH);
-
-    delay(1000);
+	if (valor < 500) {
+		delay (1000);
+		
+		digitalWrite(VERMV, LOW);
+		digitalWrite(AMARV, HIGH);
+		digitalWrite(VERDV, LOW);
+		digitalWrite(VERMP, HIGH);
+		digitalWrite(VERDP, LOW);
+		
+		delay (1500);
+		
+		digitalWrite(VERMV, HIGH);
+		digitalWrite(AMARV, LOW); 
+		digitalWrite(VERDV, LOW);
+		digitalWrite(VERMP, LOW);
+		digitalWrite(VERDP, HIGH);
+		
+		delay(1000);
+	}
 }
 
-}
